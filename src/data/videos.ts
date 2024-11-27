4c1a440bf02531f2965d04e85eeb015ee1cbfd6a
Yenupuri Hithaishi <hithaishi714@gmail.com>
2024-11-27 18:23:42 +0530

    git commit -m "new commit"

---
+++ src/data/videos.ts
@@ -0,0 +1,158 @@
+export interface Video {
+  id: string;
+  title: string;
+  description: string;
+  publishedAt: string;
+  thumbnailUrl: string;
+}
+
+export const videos: Video[] = [
+  {
+    id: "REjIo4irx-M",
+    title:
+      "Jon Jandai: Life is Easy – So Why Do We Make It Hard? | Simple Living Explained",
+    description: "Description for Video 1",
+    publishedAt: "2023-05-01T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_1/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_2",
+    title: "Video Title 2",
+    description: "Description for Video 2",
+    publishedAt: "2023-05-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_2/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_3",
+    title: "Video Title 3",
+    description: "Description for Video 3",
+    publishedAt: "2023-06-01T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_3/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+  {
+    id: "VIDEO_ID_4",
+    title: "Video Title 4",
+    description: "Description for Video 4",
+    publishedAt: "2023-06-15T00:00:00Z",
+    thumbnailUrl: "https://img.youtube.com/vi/VIDEO_ID_4/maxresdefault.jpg",
+  },
+];
